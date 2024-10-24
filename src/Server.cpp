c99821ee75121201996ae53c372362416582e672
brettsmith212 <brettsmith212@gmail.com>
2024-10-23 21:57:44 -0700

    fix

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,7 +1,11 @@
+#include <cstdlib>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <iterator>
 #include <string>
+
+#include <zlib.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every std::cout / std::cerr
@@ -35,6 +39,45 @@ int main(int argc, char *argv[]) {
       std::cerr << e.what() << '\n';
       return EXIT_FAILURE;
     }
+  } else if (command == "cat-file") {
+    if (argc < 4 || std::string(argv[2]) != "-p") {
+      std::cerr << "Missing parameter -p <hash>\n";
+      return EXIT_FAILURE;
+    }
+
+    const auto blob_hash = std::string_view(argv[3], 40);
+    const auto blob_dir = blob_hash.substr(0, 2);
+    const auto blob_file = blob_hash.substr(2);
+
+    const auto blob_path =
+        std::filesystem::path(".git") / "objects" / blob_dir / blob_file;
+
+    std::ifstream file(blob_path);
+    if (!file) {
+      std::cerr << "Failed to open " << blob_path << " file.\n";
+      return EXIT_FAILURE;
+    }
+    const auto blob_data = std::string(std::istreambuf_iterator<char>(file),
+                                       std::istreambuf_iterator<char>());
+
+    auto buf = std::string();
+    buf.resize(blob_data.size());
+    while (true) {
+      auto len = buf.size();
+      if (auto res =
+              uncompress((uint8_t *)buf.data(), &len,
+                         (const uint8_t *)blob_data.data(), blob_data.size());
+          res == Z_BUF_ERROR) {
+        buf.resize(buf.size() * 2);
+      } else if (res != Z_OK) {
+        std::cerr << "Failed to uncompress Zlib";
+        return EXIT_FAILURE;
+      } else {
+        buf.resize(len);
+        break;
+      }
+    }
+    std::cout << std::string_view(buf).substr(buf.find('\0') + 1);
   } else {
     std::cerr << "Unknown command " << command << '\n';
     return EXIT_FAILURE;
